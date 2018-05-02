0a822a2f787d3bbe8f373d906cb2259029983457
John Olivares <olivaresvjohn@gmail.com>
2018-05-02 14:46:26 -0400

    Plugin de Wordpress de EstudioPlugin de Wordpress de EstudioPlugin de Wordpress de Estudio

---
+++ john-pets.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*
+Plugin Name:  Pets
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Basic Plugin Pets
+Version:      1.0
+Author:       John
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+*/
+
+
+ ?>
